34dc95d000557ce5dd07c21afe451a3d28d4e02e
reniz-shah <reniz2810@gmail.com>
2024-08-02 16:59:58 +0530

    update alexa project

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -3,7 +3,7 @@ export const projectsData = [
     id: 1,
     name: "Alexa Skills Development",
     description:
-      "Built a real-time chat web application using resct for frontend, Strapi for backend and Mongodb as database. The application facilitate instant messaging between user and server through WebSocket communication.  The application is responsive and capable of running smoothly on various screen sizes and devices.",
+      "Enabled clients to effortlessly perform CRM tasks by providing voice instructions to Amazon Alexa. Achieved a 30% reduction in task completion time, improving user satisfaction and minimizing errors.Provided clients with hands-free access to business functionalities through voice commands, enhancing accessibility and efficiency. Enabled 50% faster task execution",
     tools: [
       "Python",
       "Odoo",
